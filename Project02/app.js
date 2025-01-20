fc9d0972e2c3b5409333169760faa366a4c0c430
Saurabh rawat <155474450+saurabhrawat-13@users.noreply.github.com>
2025-01-20 11:00:56 +0000

    Done

---
+++ Project02/app.js
@@ -0,0 +1,13 @@
+let arrow = document.getElementById("arrow");
+
+
+window.addEventListener("scroll", () => {
+    let position  = window.scrollY;
+    console.log(position);
+    
+    if (position <= 5) {
+        arrow.classList.toggle('fade-in')
+        arrow.classList.toggle('fade-out');
+    }
+    
+});
